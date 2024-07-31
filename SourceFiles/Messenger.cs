84e6b6c1e566dc454ec01f5dd6b8e2f1702b4ca3
Laurence Luo <admin@lzc.app>
2024-07-31 14:55:21 +0800

    Fix ambiguous reference of "Timer"

--- SourceFiles/Messenger.cs
+++ SourceFiles/Messenger.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
+﻿using System.Diagnostics;
 using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Windows.Forms;
 using static DarkModeForms.KeyValue;
+using Timer = System.Threading.Timer;
 
 namespace DarkModeForms {
   /* Author: BlueMystic (bluemystic.play@gmail.com)  2024 */
